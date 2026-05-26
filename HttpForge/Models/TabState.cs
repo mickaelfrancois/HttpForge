419f1a145e6dba062b9dbe9e4fe8c3070e9898e6
Mickaël FRANCOIS <mickael@francois.ovh>
2026-05-26 22:02:28 +0200

    feat: add TabState model

---
+++ HttpForge/Models/TabState.cs
@@ -0,0 +1,19 @@
+using HttpForge.Data.Entities;
+using HttpForge.Services;
+
+namespace HttpForge.Models;
+
+public class TabState
+{
+    public int RequestId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Method { get; set; } = "GET";
+    public RequestDraft Draft { get; set; } = null!;
+    public string ActiveSubTab { get; set; } = "Params";
+    public string ActiveResponseTab { get; set; } = "Body";
+    public List<UserVariableValue> UserVarValues { get; set; } = [];
+    public Dictionary<string, string> PendingPersonalValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+    public ExecutionResult? Result { get; set; }
+    public ScriptResult? ScriptResult { get; set; }
+    public bool IsSending { get; set; }
+}
